480a22163e3c229dc47f21f2f7d24e2b399daca3
Diego Goncalvez <diego.data.icloud@gmail.com>
2020-08-19 06:44:08 -0300

    hola React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Saludo from './saludo';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">    
+        <Saludo/>        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/saludo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Saludo(){
+  return(
+    <h1>¡Este es un Saludo Especial para Pili!</h1>
+  );
+
+
+}
+export default Saludo;
